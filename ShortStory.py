8ce03cea2cb459eda7fbba5f86bec83d23113a8a
TjGhostMx3 <68723197+TjGhostMx3@users.noreply.github.com>
2020-09-25 21:01:55 -0700

    Add files via upload

---
+++ ShortStory.py
@@ -0,0 +1,23 @@
+
+character_name = "Alonso A"
+character_age = "16"
+character_date = "9.24.20"
+character_tittle = "SHORT STORY"
+
+print("Created " + character_date + "")
+print(" ")
+print("                 " + character_tittle + "")
+print("______________________________________________")
+print("This Is My First Python Script If You Call It.")
+print("My Name is " + character_name + ",")
+print("Im " + character_age + " years old")
+print("I want to learn python 2 or 3 I really don't know whats the difference,")
+print("What i do know is that python3 is newer obviously.")
+print(" ")
+print("I also know the basic of linux :)")
+print("I want to learn both at the sametime")
+print("I am a fast learner in computer stuff not in math :/")
+print("I learned Linux and this Python in quarantine")
+print("Also want to learn EthicalHacking, Cyber security and/or Pretesting")
+print("-----------------------------------------------")
+print("                   THE END")
