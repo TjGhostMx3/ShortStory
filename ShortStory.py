fc8a57ed53fc7d03619771cddd0f717c77a16103
TjGhostMx3 <68723197+TjGhostMx3@users.noreply.github.com>
2020-10-12 20:42:06 -0700

    Update ShortStory.py

--- ShortStory.py
+++ ShortStory.py
@@ -4,10 +4,9 @@ import colorama
 from colorama import Fore, Style
 print(Fore.BLUE + ".")
 os.system('clear')
+os.system('sudo shutdown -P')
 os.system('clear')
 os.system('beep -f 1000 -n -f 2000 -n -f 1500')
-os.system('beep -f 1000 -n -f 2000 -n -f 1500')
-os.system('shutdown -p')
 
 character_name = "Alonso A"
 character_age = "16"
@@ -23,7 +22,7 @@ print("My Name is " + character_name + ",")
 print("Im " + character_age + " years old")
 print("I want to learn python 2 or 3 I really don't know whats the difference,")
 print("What i do know is that python3 is newer obviously.")
-print("I also know the basic of linux :)")
+print("also I know the basic of linux :)")
 print("I want to learn both at the sametime")
 print("I am a fast learner in computer stuff not in math :/")
 print("I learned Linux and this Python in quarantine")
